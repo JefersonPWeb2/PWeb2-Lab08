f1177e1da55af7cceb14489d9743c6b565946288
Jeferson <jbasurco@unsa.edu.pe>
2024-06-15 18:14:06 -0500

    Agregando las vista 'index' para enviar un correo

--- SendEmail/emails/send/views.py
+++ SendEmail/emails/send/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+
+def index(request):
+    
+    send_mail('HOLA PARA MI AMIGO',
+    'Hola. Este es un mesaje automatico.',
+    'jbasurco@unsa.edu.pe',
+    ['padeb44113@kernuo.com'],
+    fail_silently=False)
+    
+    return render(request, 'send/index.html')
