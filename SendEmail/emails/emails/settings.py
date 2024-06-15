4097b7be167021afcb1b8bb613909bf13b7d2b8f
Jeferson <jbasurco@unsa.edu.pe>
2024-06-15 18:15:58 -0500

    Configurando las credenciales para el envío de correos

--- SendEmail/emails/emails/settings.py
+++ SendEmail/emails/emails/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'send',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.hushmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jbasurco@unsa.edu.pe'
+EMAIL_HOST_PASSWORD = 'umxp fvdc vxcs jhgl'  # Utiliza la contraseña de aplicación generada para aplicaciones de terceros
